6f0be119edeb7c59996d8012654a6ece746d9a72
Vatsal Bharkhada <vatsal.bharkhada@simformsolutions.com>
2026-06-23 10:44:41 +0530

    Update: Add meta description. Add main landmark for better accessibility

--- src/App.tsx
+++ src/App.tsx
@@ -2,14 +2,14 @@ import { Wrapper } from "./components/Wrapper";
 
 function App() {
 	return (
-		<>
+		<main>
 			<header className="head">
 				<h1>useFetch demo</h1>
 			</header>
 			<div className="hook_examples">
 				<Wrapper />
 			</div>
-		</>
+		</main>
 	);
 }
 
